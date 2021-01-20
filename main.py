0e773dd0105dd80e68f5f4569c6040156f20306b
andreww23 <63858459+andreww23@users.noreply.github.com>
2021-01-20 16:24:41 +0300

    Third Commit

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import sys
 
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from random import randint
 
 
 class Example(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
         self.do_paint = False
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(500, 500, 451, 538)
+        self.setWindowTitle('Фокус')
+        self.pushButton = QPushButton(self)
+        self.pushButton.setText('Tap')
+        self.pushButton.resize(70, 20)
+        self.pushButton.move(208, 518)
         self.pushButton.clicked.connect(self.paint)
+
 
     def paintEvent(self, event):
         qp = QPainter()
@@ -24,7 +32,7 @@ class Example(QMainWindow):
         self.repaint()
 
     def draw_el(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         rc = randint(0, 100)
         qp.drawEllipse(randint(0 + rc, 451 - rc), randint(0 + rc, 538 - rc), rc, rc)
 
